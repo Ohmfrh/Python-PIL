f3f8a07f93a9351ebf8003ba0095e30d6e909091
Daniel <dancassig@gmail.com>
2015-12-10 12:21:02 -0600

    Converts a pdf to jpg image

--- ImageManipulation/pdfConvert.py
+++ ImageManipulation/pdfConvert.py
@@ -1,8 +1,20 @@
 import PythonMagick
+import Image
 
 
 def main():
+    pdf = 'pdf/1.pdf'
+    p = PythonMagick.Image()
+    p.density('200')
+    p.read(pdf)
+    p.write('img/1.jpg')
 
+    filename = 'img/test.jpg'
+    image = Image.open(filename)
+
+    image.show()
+
+    del p, image
 
 
 if __name__ == "__main__":
